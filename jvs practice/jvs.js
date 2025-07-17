80801eae97a0b0e1d5edc5d6d13ffec27a32b716
maykieieiei <fkissesmikee@gmail.com>
2025-07-17 11:46:04 +0800

    Random Number Generetor and If ststaement added

--- jvs practice/jvs.js
+++ jvs practice/jvs.js
@@ -85,6 +85,30 @@ x = Number(x);
 y = String(y);
 z = Boolean(z);
 
+    //coutner 
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
+
+
+
 // console should always be on the bottom of everything 
 // console.log(`You are ${age} years old.`);
 console.log(`Your first name is ${firstName}, and your last name is ${lastName}.`);
